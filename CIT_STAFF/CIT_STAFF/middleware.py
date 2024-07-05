79edd0c3655153f9d341658f1c561aa3d71a4256
KidusAlemayehu <kidusalemayehu705@gmail.com>
2024-07-05 11:23:42 +0300

    cors update

---
+++ CIT_STAFF/CIT_STAFF/middleware.py
@@ -0,0 +1,8 @@
+from django.utils.deprecation import MiddlewareMixin
+
+class CORSMiddleware(MiddlewareMixin):
+    def process_response(self, request, response):
+        response['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+        response['Access-Control-Allow-Credentials'] = 'true'
+        response['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+        return response

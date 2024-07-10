2987bd4ce420ba2926974019a761a644ab47ff8d
KidusAlemayehu <kidusalemayehu705@gmail.com>
2024-07-10 11:48:55 +0300

    msg url updated

--- CIT_STAFF/apps/msgs/urls.py
+++ CIT_STAFF/apps/msgs/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from .views import MessageSendAPIView, MessageListView
 
 urlpatterns = [
-    path('msg/send/', MessageSendAPIView, name="send_message"),
+    path('msg/send/', MessageSendAPIView.as_view(), name="send_message"),
     path('msg/list/', MessageListView.as_view(), name="message_list"),
 ]
